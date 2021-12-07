693c2fdb9f3609513df3890af462500c1055ba82
pjp16 <71027501+pjp16@users.noreply.github.com>
2021-12-07 18:05:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,59 +5,52 @@ import pandas as pd
 from requests_html import HTML
 from requests_html import HTMLSession
 
-session = HTMLSession()
 
-st.write("hello")
+with open('keywords.txt', 'r') as f:
+    keywords = [line.strip() for line in f]
 
+def get_source(url):
+    """Return the source code for the provided URL. 
 
+    Args: 
+        url (string): URL of the page to scrape.
 
+    Returns:
+        response (object): HTTP response object from requests_html. 
+    """
 
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
 
-# with open('keywords.txt', 'r') as f:
-#     keywords = [line.strip() for line in f]
-
-# def get_source(url):
-#     """Return the source code for the provided URL. 
-
-#     Args: 
-#         url (string): URL of the page to scrape.
-
-#     Returns:
-#         response (object): HTTP response object from requests_html. 
-#     """
-
-#     try:
-#         session = HTMLSession()
-#         response = session.get(url)
-#         return response
-
-#     except requests.exceptions.RequestException as e:
-#         print(e)
+    except requests.exceptions.RequestException as e:
+        print(e)
         
-# def scrape_google(query):
+def scrape_google(query):
 
-#     query = urllib.parse.quote_plus(query)
-#     response = get_source("https://www.google.com/search?q=" + query)
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.com/search?q=" + query)
 
-#     links = list(response.html.absolute_links)
-#     google_domains = ('https://www.google.', 
-#                       'https://google.', 
-#                       'https://webcache.googleusercontent.', 
-#                       'http://webcache.googleusercontent.', 
-#                       'https://policies.google.',
-#                       'https://support.google.',
-#                       'https://maps.google.')
+    links = list(response.html.absolute_links)
+    google_domains = ('https://www.google.', 
+                      'https://google.', 
+                      'https://webcache.googleusercontent.', 
+                      'http://webcache.googleusercontent.', 
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.')
 
-#     for url in links[:]:
-#         if url.startswith(google_domains):
-#             links.remove(url)
+    for url in links[:]:
+        if url.startswith(google_domains):
+            links.remove(url)
 
-#     return links[0:10]
+    return links[0:10]
 
-# output = {}
+output = {}
 
-# for k in keywords:
-#     urls = scrape_google(k)
-#     output[k] = urls
+for k in keywords:
+    urls = scrape_google(k)
+    output[k] = urls
 
-# df = pd.DataFrame.from_dict(output, orient='index')
+df = pd.DataFrame.from_dict(output, orient='index')
